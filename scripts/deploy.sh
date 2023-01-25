d6786479e5e09fed2feb0fd9953b9ba9c4767dcf
Marsal Martinez <marsal.martinez@halopowered.com>
2023-01-25 08:56:47 -0500

    udpate

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,25 +3,26 @@
 echo "INICIO DEL PIPELINE"
 
 ENVIRONMENT=$1
-readarray -d , -t DEPLOYMENT_APPS<<< ${{ secrets.HEROKU_APP_LIST }}
-HEROKU_API_KEY=${{ secrets.HEROKU_API_KEY }}
+DEPLOYMENT_APPS=$2
+# readarray -d , -t DEPLOYMENT_APPS<<< ${{ secrets.HEROKU_APP_LIST }}
+# HEROKU_API_KEY=${{ secrets.HEROKU_API_KEY }}
 
-echo "Heroku app list" ${{secrets.HEROKU_APP_LIST}}
+# echo "Heroku app list" ${{secrets.HEROKU_APP_LIST}}
 echo "Heroku deployment apps" ${DEPLOYMENT_APPS}
 
-if [[ "production" == "${ENVIRONMENT}" ]]; then
-  HEROKU_APP=$(echo "${DEPLOYMENT_APPS[0]}" | tr --delete '\n')
-elif [[ "staging" == "${ENVIRONMENT}" ]]; then
-  HEROKU_APP=$(echo "${DEPLOYMENT_APPS[1]}" | tr --delete '\n')
-elif [[ "development" == "${ENVIRONMENT}" ]]; then
-  HEROKU_APP=$(echo "${DEPLOYMENT_APPS[2]}" | tr --delete '\n')
-fi
+# if [[ "production" == "${ENVIRONMENT}" ]]; then
+#   HEROKU_APP=$(echo "${DEPLOYMENT_APPS[0]}" | tr --delete '\n')
+# elif [[ "staging" == "${ENVIRONMENT}" ]]; then
+#   HEROKU_APP=$(echo "${DEPLOYMENT_APPS[1]}" | tr --delete '\n')
+# elif [[ "development" == "${ENVIRONMENT}" ]]; then
+#   HEROKU_APP=$(echo "${DEPLOYMENT_APPS[2]}" | tr --delete '\n')
+# fi
 
-if [[ "" == "${HEROKU_APP}" ]]; then
-  echo "HEROKU_APP should be specified"
-  exit 1
-fi
+# if [[ "" == "${HEROKU_APP}" ]]; then
+#   echo "HEROKU_APP should be specified"
+#   exit 1
+# fi
 
-echo "Heroku app" ${HEROKU_APP}
-echo "Heroku api key" ${HEROKU_API_KEY}
-echo "Start the deployment"
+# echo "Heroku app" ${HEROKU_APP}
+# echo "Heroku api key" ${HEROKU_API_KEY}
+# echo "Start the deployment"
