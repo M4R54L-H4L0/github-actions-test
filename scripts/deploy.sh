2a86dffd42c8ec68c25d95766e7502d81d09154e
Marsal Martinez <marsal.martinez@halopowered.com>
2023-01-25 08:42:12 -0500

    Added script to get secrets

---
+++ scripts/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ENVIRONMENT=$1
+readarray -d , -t DEPLOYMENT_APPS<<< ${{secrets.HEROKU_APP_LIST}}
+HEROKU_API_KEY=${{secrets.HEROKU_API_KEY}}
+
+echo "Heroku app list" ${{secrets.HEROKU_APP_LIST}}
+echo "Heroku deployment apps" ${DEPLOYMENT_APPS}
+
+if [[ "production" == "${ENVIRONMENT}" ]]; then
+  HEROKU_APP=$(echo "${DEPLOYMENT_APPS[0]}" | tr --delete '\n')
+elif [[ "staging" == "${ENVIRONMENT}" ]]; then
+  HEROKU_APP=$(echo "${DEPLOYMENT_APPS[1]}" | tr --delete '\n')
+elif [[ "development" == "${ENVIRONMENT}" ]]; then
+  HEROKU_APP=$(echo "${DEPLOYMENT_APPS[2]}" | tr --delete '\n')
+fi
+
+if [[ "" == "${HEROKU_APP}" ]]; then
+  echo "HEROKU_APP should be specified"
+  exit 1
+fi
+
+echo "Heroku app" ${HEROKU_APP}
+echo "Heroku api key" ${HEROKU_API_KEY}
+echo "Start the deployment"
